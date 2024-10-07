809c54a0c43864a6ede084bbe60a5d5cfd69b7de
Arnj121 <arnj121@gmail.com>
2024-10-07 00:23:55 -0500

    search

--- searchServer.py
+++ searchServer.py
@@ -1,24 +1,29 @@
 from flask import Flask, request
 from db import *
-import json
-from bson import json_util
 
 app=Flask(__name__)
 
 @app.get('/search')
 def search():
     query=request.args.get('q')
-    print(query)
-
     result = collection.find({
         'data':{
         '$regex':f"^{query}",'$options':"i"
         }
     },{'filename':1,'_id':0,'data':1,'rawdata':1})
     result=list(result)
-    # json_data = json.dumps(result, default=json_util.default)
-    print(result)
     return {'response':result}
+
+@app.get('/getfiledata')
+def getFileData():
+    query=request.args.get('q')
+    result = collection.find({
+        'filename':{
+            '$regex':f"{query}","$options":'i'
+        }
+    })
+    return {'response':result}
+
 
 app.run(host=host,port=searchport)
 
